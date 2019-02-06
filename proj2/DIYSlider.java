8f0bb30d30c784935ad28f20866a4491a426a849
Jeff Reimschussel <20570258+jtreim@users.noreply.github.com>
2019-02-05 18:37:21 -0700

    Implement generalized slider

--- proj2/DIYSlider.java
+++ proj2/DIYSlider.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
 
 public class DIYSlider extends View {
     public interface OnDIYSliderChangeListener {
-        void onStartObservingTouch(DIYSlider slider, ArrayList<Integer> values);
-        void onValueChanged(DIYSlider slider, ArrayList<Integer> values);
-        void onStopObservingTouch(DIYSlider slider, ArrayList<Integer> values);
+        void onStartObservingTouch(DIYSlider slider, ArrayList<Float> values);
+        void onValueChanged(DIYSlider slider, ArrayList<Float> values);
+        void onStopObservingTouch(DIYSlider slider, ArrayList<Float> values);
     }
 
     private class SliderThumb {
@@ -78,7 +78,7 @@ public class DIYSlider extends View {
 
     private final float LINE_Y = 100f;
     private final float LINE_STROKE_WIDTH = 25f;
-    private final int LINE_ACTIVE_COLOR = getResources().getColor(R.color.colorAccent);
+    private final int LINE_ACTIVE_COLOR = getResources().getColor(R.color.colorPrimary);
     private final int LINE_INACTIVE_COLOR = Color.LTGRAY;
 
     private final float THUMB_RADIUS = 50f;
@@ -86,7 +86,7 @@ public class DIYSlider extends View {
     private final int THUMB_INACTIVE_COLOR = getResources().getColor(R.color.colorPrimary);
     private final int THUMB_ACTIVE_COLOR = getResources().getColor(R.color.colorPrimaryDark);
 
-    private int mWidth, mHeight;
+    private int mWidth, mWidthPercent, mHeight;
     private ArrayList<SliderThumb> mThumbs = new ArrayList<>();
     private PointF mLineStart, mLineEnd;
     private Paint mLinePaintActive, mLinePaintInactive;
@@ -95,7 +95,9 @@ public class DIYSlider extends View {
 
     private SliderThumb mActiveThumb;
 
-    public DIYSlider(Context context, int thumbCount, float min, float max){
+    private OnDIYSliderChangeListener mListener;
+
+    public DIYSlider(Context context, int thumbCount, float min, float max, int widthPercent){
         super(context);
 
         mLineStart = new PointF(PADDING, LINE_Y);
@@ -114,37 +116,69 @@ public class DIYSlider extends View {
         mMin = min;
         mMax = max;
 
+        // Default width if nothing happens.
         mWidth = 500;
+
+        // Force 0 <= widthPercent <= 100
+        mWidthPercent = widthPercent;
+        if(mWidthPercent > 100) {
+            mWidthPercent = 100;
+        } else if(mWidthPercent < 0){
+            mWidthPercent = 0;
+        }
+
+        // Default height if nothing happens.
         mHeight = (int)(THUMB_RADIUS * 2.0f + PADDING * 2.0f);
 
-        // Determine equidistant spacing between thumbs along line, without putting thumbs on ends.
-        float thumbSpacing = mWidth/(float)(thumbCount+2);
-
+        // Default thumb positions/values if nothing happens.
         for(int i = 0; i < thumbCount; i++){
-            float x = thumbSpacing * (i + 1) + PADDING;
-            PointF pos = new PointF(x, LINE_Y);
-            float value = getPositionValueOnSlider(pos);
-            SliderThumb t = new SliderThumb(pos, value);
+            PointF pos = new PointF(PADDING, LINE_Y);
+            SliderThumb t = new SliderThumb(pos, mMin);
             t.setRadius(THUMB_RADIUS);
             mThumbs.add(t);
         }
         invalidate();
     }
 
+    public void setListener(OnDIYSliderChangeListener listener){
+        mListener = listener;
+        mListener.onStartObservingTouch(this, getThumbValues());
+    }
+
+    public void removeListener(){
+        mListener.onStopObservingTouch(this, getThumbValues());
+        mListener = null;
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
         mWidth = 500;
-        int widthPercent = 100;
         if(MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.UNSPECIFIED){
-            mWidth = MeasureSpec.getSize(widthMeasureSpec) * widthPercent/100;
+            mWidth = MeasureSpec.getSize(widthMeasureSpec) * mWidthPercent/100 - (int)PADDING;
         }
 
+        mLineEnd.x = mWidth;
 
         mHeight = (int)(THUMB_RADIUS * 2.0f + PADDING * 2.0f);
         if(MeasureSpec.getMode(heightMeasureSpec) != MeasureSpec.UNSPECIFIED){
             mHeight = Math.min(mHeight, MeasureSpec.getSize(heightMeasureSpec));
         }
-        setMeasuredDimension(mWidth, mHeight);
+        setMeasuredDimension(mWidth + (int)PADDING, mHeight);
+
+        invalidate();
+    }
+
+    public void setup(){
+        // Determine equidistant spacing between thumbs along line
+        float thumbSpacing = mWidth/(float)(mThumbs.size());
+
+        for(int i = 0; i < mThumbs.size(); i++){
+            float x = thumbSpacing * (i) + PADDING;
+            SliderThumb t = mThumbs.get(i);
+            t.setX(x);
+            float value = getPositionValueOnSlider(t.mPos);
+            t.setValue(value);
+        }
         invalidate();
     }
 
@@ -153,7 +187,7 @@ public class DIYSlider extends View {
         super.onDraw(canvas);
 
         // Check number of thumbs in slider to determine where "active" line color starts/ends.
-        // More than 1 thumb: "active" is between first and last thumbs.
+        // More than 1 thumb: "active" is between original first and last thumbs.
         // Exactly 1 thumb: "active" is from beginning of line to thumb.
         // 0 thumbs: No part of the line is "active".
         PointF activeLineStart, activeLineEnd;
@@ -187,16 +221,14 @@ public class DIYSlider extends View {
     }
 
     private void clamp(){
-        if (mActiveThumb != null) {
-            if (mActiveThumb.getX() > mLineEnd.x) {
-                mActiveThumb.setX(mLineEnd.x);
-            } else if (mActiveThumb.getX() < mLineStart.x) {
-                mActiveThumb.setX(mLineStart.x);
-            }
+        if (mActiveThumb.getX() > mLineEnd.x) {
+            mActiveThumb.setX(mLineEnd.x);
+        } else if (mActiveThumb.getX() < mLineStart.x) {
+            mActiveThumb.setX(mLineStart.x);
+        }
 
-            if (mActiveThumb.getY() != LINE_Y) {
-                mActiveThumb.setY(LINE_Y);
-            }
+        if (mActiveThumb.getY() != LINE_Y) {
+            mActiveThumb.setY(LINE_Y);
         }
     }
 
@@ -208,15 +240,18 @@ public class DIYSlider extends View {
     }
 
     private float getPositionValueOnSlider(PointF pos){
-        float value = (pos.x - mLineStart.x) / (mWidth);
+        float value = (pos.x - mLineStart.x) / (mWidth - PADDING);
         value = value * (mMax - mMin) + mMin;
         return value;
     }
 
     private void updateThumbValue(){
-        if(mActiveThumb != null) {
-            float value = getPositionValueOnSlider(mActiveThumb.mPos);
-            mActiveThumb.setValue(value);
+        float value = getPositionValueOnSlider(mActiveThumb.mPos);
+        mActiveThumb.setValue(value);
+
+        // Only bug the listener if there's a thumb to report.
+        if(mListener != null && mThumbs.size() > 0){
+            mListener.onValueChanged(this, getThumbValues());
         }
     }
 
@@ -239,10 +274,15 @@ public class DIYSlider extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 PointF eventPoint = new PointF(event.getX(), event.getY());
-                if(isOnLine(eventPoint)){
+                if(isOnLine(eventPoint) && mThumbs.size() > 0){
                     mActiveThumb = getClosestThumb(eventPoint);
                     mActiveThumb.setColor(THUMB_ACTIVE_COLOR);
                     mActiveThumb.setActive(true);
+
+                    mActiveThumb.setX(event.getX());
+                    mActiveThumb.setY(event.getY());
+                    clamp();
+                    updateThumbValue();
                 }
                 break;
 
@@ -250,6 +290,8 @@ public class DIYSlider extends View {
                 if(mActiveThumb != null){
                     mActiveThumb.setX(event.getX());
                     mActiveThumb.setY(event.getY());
+                    clamp();
+                    updateThumbValue();
                 }
                 break;
 
@@ -257,6 +299,10 @@ public class DIYSlider extends View {
                 if(mActiveThumb != null){
                     mActiveThumb.setX(event.getX());
                     mActiveThumb.setY(event.getY());
+                    clamp();
+                    updateThumbValue();
+
+                    // We're done listening to this nonsense.
                     mActiveThumb.setColor(THUMB_INACTIVE_COLOR);
                     mActiveThumb.setActive(false);
                     mActiveThumb = null;
@@ -264,8 +310,6 @@ public class DIYSlider extends View {
                 break;
         }
 
-        clamp();
-        updateThumbValue();
         invalidate();
         return true;
     }
--- proj2/MainActivity.java
+++ proj2/MainActivity.java
@@ -3,6 +3,7 @@ package reimschussel.diyslider;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,24 +14,41 @@ public class MainActivity extends AppCompatActivity implements DIYSlider.OnDIYSl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DIYSlider slider = new DIYSlider(this, 2, 10, 50);
+        final DIYSlider slider = new DIYSlider(this, 3, 100, 200, 100);
+        slider.setListener(this);
 
         FrameLayout layout = (FrameLayout) findViewById(R.id.slider_placeholder);
         layout.addView(slider);
+
+        slider.post(new Runnable() {
+            @Override
+            public void run() {
+                slider.setup();
+            }
+        });
     }
 
     @Override
-    public void onStartObservingTouch(DIYSlider slider, ArrayList<Integer> values) {
-
+    public void onStartObservingTouch(DIYSlider slider, ArrayList<Float> values) {
+        TextView sliderValues = findViewById(R.id.thumbValuesText);
+        sliderValues.setText("Ready");
     }
 
     @Override
-    public void onValueChanged(DIYSlider slider, ArrayList<Integer> values) {
+    public void onValueChanged(DIYSlider slider, ArrayList<Float> values) {
+        TextView sliderValues = findViewById(R.id.thumbValuesText);
+        StringBuilder text = new StringBuilder();
+        for(int i = 0; i < values.size(); i++){
+            text.append("Thumb " + Integer.toString(i+1) + ": ");
+            text.append(values.get(i));
+            text.append("\n");
+        }
 
+        sliderValues.setText(text);
     }
 
     @Override
-    public void onStopObservingTouch(DIYSlider slider, ArrayList<Integer> values) {
+    public void onStopObservingTouch(DIYSlider slider, ArrayList<Float> values) {
 
     }
 }
